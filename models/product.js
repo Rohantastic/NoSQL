ba911dcd7a8e70174d99157bc3b75cc831f9ad62
rohantastic <rohantastic@outlook.com>
2023-09-12 19:36:30 +0530

    update

--- models/product.js
+++ models/product.js
@@ -1,18 +1,46 @@
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title,price,description,imageUrl){
+  constructor(title, price, description, imageUrl) {
     this.title = title;
     this.price = price;
     this.description = description;
-    this.imageUrl = imageUrl
+    this.imageUrl = imageUrl;
   }
 
-  save(){
+  save() {
     const db = getDb();
-    return db.collection('products').insertOne(this).
-    then((result)=>{console.log(result)})
-    .catch((err)=>{
+    return db.collection('products')
+      .insertOne(this)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static fetchAll() {
+    const db = getDb(); // Obtain the db reference here
+    return db.collection('products')
+      .find()
+      .toArray()
+      .then((result) => {
+        console.log(result);
+        return result;
+      })
+      .catch((err) => {
+        console.log(err);
+        throw err; // Throw the error to handle it in the calling code
+      });
+  }
+
+  static findById(prodId){
+    const db = getDb();
+    return db.collection('products').find({_id: prodId}).next().then((result)=>{
+      console.log(result);
+      return result;
+    }).catch((err)=>{
       console.log(err);
     });
   }
