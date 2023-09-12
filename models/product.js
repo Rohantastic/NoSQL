8d7425ad95671c124017be695c7210da20a40a75
rohantastic <rohantastic@outlook.com>
2023-09-12 20:44:31 +0530

    update

--- models/product.js
+++ models/product.js
@@ -2,18 +2,24 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl,id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   save() {
     const db = getDb();
-    return db.collection('products')
-      .insertOne(this)
-      .then((result) => {
+    let dbOp;
+    if(this._id){
+      dbOp = db.collection('products').updateOne({_id: this._id}, {$set: this});
+    }else{
+      dbOp = db.collection('products').insertOne(this);
+    }
+    
+    return dbOp.then((result) => {
         console.log(result);
       })
       .catch((err) => {
@@ -44,6 +50,16 @@ class Product {
     }).catch((err)=>{
       console.log(err);
     });
+  }
+
+  static deleteById(prodId){
+    const db = getDb();
+    return db.collection('products').deleteOne({_id: new mongodb.ObjectId(prodId)}).then(()=>{
+      console.log("deleted");
+    }).
+    catch((err)=>{
+      console.log(err);
+    })
   }
 }
 
