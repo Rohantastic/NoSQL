109b032cc55a8933e445eb35432587c82c0e8e40
rohantastic <rohantastic@outlook.com>
2023-09-13 14:42:26 +0530

    one

--- app.js
+++ app.js
@@ -16,14 +16,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById(1)
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
  app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -31,8 +31,21 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 
-mongoose.connect('mongodb+srv://root:root@cluster0.ioata4v.mongodb.net/?retryWrites=true&w=majority').then(()=>{
-  app.listen(3000);
+mongoose.connect('mongodb+srv://root:root@cluster0.ioata4v.mongodb.net/?retryWrites=true&w=majority').then((result)=>{
+  
+User.findOne().then(user=>{
+  if(!user){
+    const user = new User({
+      name: "rohan",
+      email : 'afef@g.com',
+      cart:{
+        items:[]
+      }
+    })
+    user.save();
+  }
+});
+app.listen(3000);
 }).catch(err=>{
   console.log(err);
 })
--- controllers/admin.js
+++ controllers/admin.js
@@ -16,7 +16,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product({title: title, price:price, description:description, imageUrl:imageUrl});
+  const product = new Product({title: title, price:price, description:description, imageUrl:imageUrl, userId: req.user});
   product.save()
     .then(result => {
       // console.log(result);
@@ -57,9 +57,14 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, prodId);
-
-  product.save().then(result => {
+  
+  Product.findById(prodId).then((product)=>{
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.description = updatedDesc;
+    product.imageUrl = updatedImageUrl
+    return product.save()
+  }).then(result => {
     console.log('UPDATED PRODUCT!');
     res.redirect('/admin/products');
   })
@@ -67,8 +72,11 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find().
+  select('title price -_id').
+    populate('userId','name')
     .then(products => {
+
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
@@ -80,7 +88,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
